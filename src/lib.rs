81e02b57a9940c1f4ce2c5dee3a688eaf7f33114
Andrei Litvin <andy314@gmail.com>
2022-06-10 10:41:05 -0400

    Reformat return codes a bit, prepare for value reading

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 pub mod types;
 
 use byteorder::{ByteOrder, LittleEndian};
-use types::{ContainerType, TagType};
+use types::{ContainerType, ElementType, TagType};
 
 /// Represents an actual value read from a TLV record
 pub enum Value<'a> {
@@ -27,6 +27,14 @@ pub struct Record<'a> {
     pub value: Value<'a>,
 }
 
+/// Represents an incremental parsing result containing
+/// some data and the remaining parse buffer
+#[derive(Debug, PartialEq)]
+pub(crate) struct IncrementalParseResult<'a, T> {
+    pub(crate) parsed: T,
+    pub(crate) remaining_input: &'a [u8],
+}
+
 pub struct Parser<'a> {
     data: &'a [u8],
 }
@@ -46,7 +54,10 @@ impl<'a> Parser<'a> {
     }
 
     /// Returns the undelying tag and rest of the data
-    pub(crate) fn read_tag_value(tag_type: TagType, data: &[u8]) -> (Option<u64>, &[u8]) {
+    pub(crate) fn read_tag_value(
+        tag_type: TagType,
+        data: &[u8],
+    ) -> Option<IncrementalParseResult<u64>> {
         let tag_length = match tag_type {
             TagType::Anonymous => 0,
             TagType::ContextSpecific1byte => 1,
@@ -58,18 +69,25 @@ impl<'a> Parser<'a> {
 
         if data.len() < tag_length {
             // Cannot parse, return nothing and do not consume the data
-            return (None, data);
+            return None;
         }
 
-        let (buf, rest) = data.split_at(tag_length);
+        let (buf, remaining_input) = data.split_at(tag_length);
+        let parsed = match tag_length {
+            0 => 0u64,
+            nbytes => LittleEndian::read_uint(buf, nbytes),
+        };
 
-        (
-            Some(match tag_length {
-                0 => 0u64,
-                nbytes => LittleEndian::read_uint(buf, nbytes),
-            }),
-            rest,
-        )
+        Some(IncrementalParseResult {
+            parsed,
+            remaining_input,
+        })
+    }
+
+    pub(crate) fn read_value(
+        element_type: ElementType,
+    ) -> Option<IncrementalParseResult<'a, Value<'a>>> {
+        todo!()
     }
 }
 
@@ -81,12 +99,13 @@ impl<'a> Iterator for Parser<'a> {
             None => None,
             Some((control, rest)) => {
                 let tag_type = TagType::for_control(*control);
-                let (tag_value, rest) = Parser::read_tag_value(tag_type, rest);
 
-                if tag_value.is_none() {
-                    return None;
-                }
-                let tag_value = tag_value.unwrap();
+                let result = Parser::read_tag_value(tag_type, rest)?;
+
+                let tag_value = result.parsed;
+                let rest = result.remaining_input;
+
+                let value_type = ElementType::for_control(*control)?;
 
                 // FIXME: read actual value
 
@@ -117,91 +136,115 @@ mod tests {
         let empty = [].as_slice();
         assert_eq!(
             Parser::read_tag_value(TagType::Anonymous, empty),
-            (Some(0), empty)
+            Some(IncrementalParseResult {
+                parsed: 0,
+                remaining_input: empty
+            })
         );
 
         let some_bytes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].as_slice();
         assert_eq!(
             Parser::read_tag_value(TagType::Anonymous, some_bytes),
-            (Some(0), some_bytes)
+            Some(IncrementalParseResult {
+                parsed: 0,
+                remaining_input: some_bytes
+            })
         );
 
         assert_eq!(
             Parser::read_tag_value(TagType::ContextSpecific1byte, some_bytes),
-            (Some(0x01), [2, 3, 4, 5, 6, 7, 8, 9, 10].as_slice())
+            Some(IncrementalParseResult {
+                parsed: 0x01,
+                remaining_input: [2, 3, 4, 5, 6, 7, 8, 9, 10].as_slice()
+            })
         );
 
         assert_eq!(
             Parser::read_tag_value(TagType::CommonProfile2byte, some_bytes),
-            (Some(0x0201), [3, 4, 5, 6, 7, 8, 9, 10].as_slice())
+            Some(IncrementalParseResult {
+                parsed: 0x0201,
+                remaining_input: [3, 4, 5, 6, 7, 8, 9, 10].as_slice()
+            })
         );
 
         assert_eq!(
             Parser::read_tag_value(TagType::Implicit2byte, some_bytes),
-            (Some(0x0201), [3, 4, 5, 6, 7, 8, 9, 10].as_slice())
+            Some(IncrementalParseResult {
+                parsed: 0x0201,
+                remaining_input: [3, 4, 5, 6, 7, 8, 9, 10].as_slice()
+            })
         );
 
         assert_eq!(
             Parser::read_tag_value(TagType::Implicit4byte, some_bytes),
-            (Some(0x04030201), [5, 6, 7, 8, 9, 10].as_slice())
+            Some(IncrementalParseResult {
+                parsed: 0x04030201,
+                remaining_input: [5, 6, 7, 8, 9, 10].as_slice()
+            })
         );
 
         assert_eq!(
             Parser::read_tag_value(TagType::CommonProfile4byte, some_bytes),
-            (Some(0x04030201), [5, 6, 7, 8, 9, 10].as_slice())
+            Some(IncrementalParseResult {
+                parsed: 0x04030201,
+                remaining_input: [5, 6, 7, 8, 9, 10].as_slice()
+            })
         );
 
         assert_eq!(
             Parser::read_tag_value(TagType::FullyQualified6byte, some_bytes),
-            (Some(0x060504030201), [7, 8, 9, 10].as_slice())
+            Some(IncrementalParseResult {
+                parsed: 0x060504030201,
+                remaining_input: [7, 8, 9, 10].as_slice()
+            })
         );
 
         assert_eq!(
             Parser::read_tag_value(TagType::FullyQualified8byte, some_bytes),
-            (Some(0x0807060504030201), [9, 10].as_slice())
+            Some(IncrementalParseResult {
+                parsed: 0x0807060504030201,
+                remaining_input: [9, 10].as_slice()
+            })
         );
     }
 
     #[test]
     fn read_tag_value_fails_on_short() {
         let empty = [].as_slice();
-        assert_eq!(
-            Parser::read_tag_value(TagType::Implicit2byte, empty),
-            (None, empty)
-        );
+        assert_eq!(Parser::read_tag_value(TagType::Implicit2byte, empty), None);
 
         assert_eq!(
             Parser::read_tag_value(TagType::CommonProfile2byte, empty),
-            (None, empty)
+            None
         );
 
         assert_eq!(
             Parser::read_tag_value(TagType::FullyQualified6byte, empty),
-            (None, empty)
+            None
         );
 
         let one_byte = [1].as_slice();
 
         assert_eq!(
             Parser::read_tag_value(TagType::CommonProfile2byte, one_byte),
-            (None, one_byte)
+            None
         );
 
         assert_eq!(
             Parser::read_tag_value(TagType::FullyQualified6byte, one_byte),
-            (None, one_byte)
+            None
         );
-        
-        let four_bytes = [1,2,3,4].as_slice();
+
+        let four_bytes = [1, 2, 3, 4].as_slice();
 
         assert_eq!(
             Parser::read_tag_value(TagType::FullyQualified6byte, four_bytes),
-            (None, four_bytes)
+            None
         );
 
         assert_eq!(
             Parser::read_tag_value(TagType::FullyQualified8byte, four_bytes),
-            (None, four_bytes)
+            None
         );
     }
 }
