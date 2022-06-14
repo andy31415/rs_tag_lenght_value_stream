5ed60bc9e4acd67975ff1a5d34d146941a1cd937
Andrei Litvin <andy314@gmail.com>
2022-06-14 09:24:16 -0400

    Fix compare for NaN values

--- fuzz/fuzz_targets/parse_and_write.rs
+++ fuzz/fuzz_targets/parse_and_write.rs
@@ -1,38 +1,67 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use tag_length_value_stream::Parser;
-use tag_length_value_stream::TlvBytes;
 use streaming_iterator::StreamingIterator;
+use tag_length_value_stream::{Parser, Record, TlvBytes, Value};
+
+/// Compare records while comparing NaN to NaN.
+fn check_same(a: Option<Record>, b: Option<Record>) {
+    match (a, b) {
+        (
+            Some(Record {
+                tag: ta,
+                value: Value::Double(va),
+            }),
+            Some(Record {
+                tag: tb,
+                value: Value::Double(vb),
+            }),
+        ) => {
+            assert_eq!(ta, tb);
+            assert!((va == vb) || (va.is_nan() && vb.is_nan()));
+        }
+        (
+            Some(Record {
+                tag: ta,
+                value: Value::Float(va),
+            }),
+            Some(Record {
+                tag: tb,
+                value: Value::Float(vb),
+            }),
+        ) => {
+            assert_eq!(ta, tb);
+            assert!((va == vb) || (va.is_nan() && vb.is_nan()));
+        }
+        _ => assert_eq!(a, b),
+    }
+}
 
 fuzz_target!(|data: &[u8]| {
-
     let parser = Parser::new(data);
     let mut streamer = streaming_iterator::convert(parser);
     let mut generator = TlvBytes::new(&mut streamer);
-    
+
     let mut rewritten = vec![];
 
     while let Some(data) = generator.next() {
-       for b in data {
-           rewritten.push(*b);
-       }
+        for b in data {
+            rewritten.push(*b);
+        }
     }
 
     // Both streams should be identical
     let mut original = Parser::new(data);
-    let mut updated  = Parser::new(rewritten.as_slice());
-    
+    let mut updated = Parser::new(rewritten.as_slice());
+
     loop {
         let a = original.next();
         let b = updated.next();
-        
-        assert_eq!(a, b);
+
+        check_same(a, b);
 
         if a.is_none() {
             break;
         }
     }
-    
-    
 });
